9e87b6c3b692e481bca17a809f3eab102cbc90e5
Zach Hammer <zhammer@seatgeek.com>
2019-11-06 10:19:34 -0500

    Add hypothesis tests for soundex

--- test/test_fuzzy.py
+++ test/test_fuzzy.py
@@ -2,36 +2,81 @@
 
 from __future__ import unicode_literals
 
-import pytest
 import ctypes
+import string
+
+import pytest
+from hypothesis import given, note
+from hypothesis import strategies as st
+from hypothesis import settings
 
 import fuzzy
 
 
-def test_soundex_does_not_mutate_strings():
-	phrase = 'FancyFree'
-	fuzzy.Soundex(4)(phrase)
-	buffer = ctypes.c_char_p(phrase.encode())
-	assert buffer.value.decode() == "FancyFree"
+class PythonSoundex(object):
+    # from sglib 5.0.0 https://github.com/seatgeek/sglib-py/commit/a8fddacb09c8a3edfada667f4244521d1dc68f69
+    def __init__(self, size):
+        self.size = size
+        self.map = "01230120022455012623010202"
+
+        self.memoized_outputs = {}
+
+    def __call__(self, s):
+        if s in self.memoized_outputs:
+            return self.memoized_outputs[s]
+
+        written = 0
+
+        ords = [ord(i) for i in s]
+        out = []
+        ls = len(s)
+        for i in range(0, ls):
+            c = ords[i]
+            if c >= 97 and c <= 122:
+                c = c - 32
+            if c >= 65 and c <= 90:
+                if written == 0:
+                    out.append(chr(c))
+                    written = written + 1
+                elif self.map[c - 65] != '0' and (written == 1 or
+                                                 out[written - 1] != self.map[c - 65]):
+                    out.append(self.map[c - 65])
+                    written = written + 1
+            if written == self.size:
+                break
+
+        for i in range(written, self.size):
+            out.append('0')
+
+        ret = ''.join(out)
+        self.memoized_outputs[s] = ret
+        return ret
 
 
-@pytest.mark.xfail(reason="issue #14")
-def test_soundex_result():
-	phrase = 'FancyFree'
-	res = fuzzy.Soundex(4)(phrase)
-	assert res == 'F521'
+cython_input_strategy = st.text(alphabet=string.ascii_letters + string.digits, min_size=1, max_size=30)
 
 
-@pytest.mark.xfail(reason="issue #14")
-def test_soundex_Test():
-	assert fuzzy.Soundex(8)('Test') == 'T23'
+@given(cython_input_strategy, st.integers(1, 10))
+def test_soundex(text, size):
+    # copy over original text to make sure soundex doesn't mutate text
+    original_text = text[:]
+
+    cython_out = fuzzy.Soundex(size)(text)
+
+    assert text == original_text
+    assert cython_out == PythonSoundex(size)(text)
 
 
-@pytest.mark.xfail(reason="issue #15")
-def test_soundex_non_ascii():
-	assert fuzzy.Soundex(8)('Jéroboam') == 'J615'
+@given(st.lists(cython_input_strategy, min_size=100, max_size=200, unique=True), st.integers(1, 10))
+def test_soundex_repeated_uses(texts, size):
+    cython_soundex = fuzzy.Soundex(size)
+    python_soundex = PythonSoundex(size)
 
+    for text in texts:
+        print(text)
+        original_text = text[:]
 
-def test_DMetaphone():
-	m = fuzzy.DMetaphone()
-	assert m("mayer") == [b'MR', None]
+        cython_out = cython_soundex(text)
+        assert original_text == text
+        assert cython_out == python_soundex(text)
+
